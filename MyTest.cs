65095a107926ffa3369dfd472203eb6451b4473a
Sangbu Kim <SKIM175@art.edu>
2018-08-12 21:32:46 -0700

    added Add and Subtract methods!

--- MyTest.cs
+++ MyTest.cs
@@ -3,4 +3,26 @@
 public class TestClass
 {
     public TestClass() { }
+
+    /// <summary>
+    /// adds a with b
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    private int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    /// <summary>
+    /// subtract b from a
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    private int Subtract(int a, int b)
+    {
+        return a - b;
+    }
 }
